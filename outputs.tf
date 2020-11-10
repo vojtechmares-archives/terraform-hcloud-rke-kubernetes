1d2c08e5aa5df214c5cb769300ce16051b381aed
Vojtech Mares <iam@vojtechmares.com>
2020-11-10 17:24:36 +0100

    variables.tf, outputs.tf - added descriptions for all Terraform variables and outputs

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,10 @@
 output "load_balancer_ipv4" {
-  value = hcloud_load_balancer.lb.ipv4
+  value       = hcloud_load_balancer.lb.ipv4
+  description = "Public IPv4 of a load balancer"
 }
 
 output "kube_config_yaml" {
-  value     = rke_cluster.cluster.kube_config_yaml
-  sensitive = true
+  value       = rke_cluster.cluster.kube_config_yaml
+  sensitive   = true
+  description = "Kubeconfig for a cluster (in an YAML format)"
 }
--- variables.tf
+++ variables.tf
@@ -1,39 +1,51 @@
 // Provider variables
 variable "hcloud_token" {
-  type = string
+  type        = string
+  description = "API Token for Hetzner Cloud"
 }
 
 // Module specific variables
 variable "ssh_public_key" {
-  type = string
+  type        = string
+  description = "SSH Public key uploaded to nodes for remote access and RKE"
 }
 variable "ssh_private_key" {
-  type = string
+  type        = string
+  description = "SSH Private key (pair with `ssh_public_key`) for RKE to access nodes to install Kubernetes components"
 }
 variable "location" {
-  type = string
+  type        = string
+  description = "Hetzner Cloud datacenter to deploy servers and load balancer to"
 }
 variable "node_count" {
-  type = number
+  type        = number
+  description = "Number of servers (nodes) for Kubernetes cluster"
 }
 variable "node_type" {
-  type = string
+  type        = string
+  description = "Type of server (size e.g. `cpx31`)"
 }
 variable "node_image" {
-  type = string
+  type        = string
+  description = "Server image to choose base operating system (e.g. `ubuntu-20.04`)"
 }
 variable "load_balancer_type" {
-  type = string
+  type        = string
+  description = "Type of load balancer (e.g. `lb11`)"
 }
 variable "kubernetes_version" {
-  type = string
+  type        = string
+  description = "Version of Kubernetes cluster created by RKE (e.g `v1.19.3-rancher1-1`)"
 }
 variable "kubernetes_cluster_name" {
-  type = string
+  type        = string
+  description = "Name of Kubernetes cluster"
 }
 variable "kubernetes_upgrade_strategy_drain" {
-  type = bool
+  type        = bool
+  description = "Kubernetes should drain node on upgrade"
 }
 variable "kubernetes_upgrade_strategy_max_unavailable_workers" {
-  type = string
+  type        = string
+  description = "Maximum of Kubernetes unavailable workers"
 }
