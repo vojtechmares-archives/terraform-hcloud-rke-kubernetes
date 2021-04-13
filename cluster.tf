c48863aba42f99a1fa687367f11e6be1a6a30bc1
Vojtech Mares <iam@vojtechmares.com>
2021-04-14 01:20:58 +0200

    refactor(cluster): clean up cluster.tf

--- cluster.tf
+++ cluster.tf
@@ -22,44 +22,10 @@ resource "rke_cluster" "cluster" {
       address = nodes.value.ipv4_address
       user    = "root"
       role    = ["controlplane", "worker", "etcd"]
-      ssh_key = var.ssh_private_key
-      port    = 22
     }
   }
 
   kubernetes_version = var.kubernetes_version
-
-  services {
-    kube_api {
-      service_cluster_ip_range = "10.43.0.0/16"
-    }
-
-    kube_controller {
-      cluster_cidr             = "10.42.0.0/16"
-      service_cluster_ip_range = "10.43.0.0/16"
-    }
-
-    kubelet {
-      cluster_domain               = "cluster.local"
-      cluster_dns_server           = "10.43.0.10"
-      fail_swap_on                 = false
-      generate_serving_certificate = false
-    }
-
-    etcd {}
-
-    scheduler {}
-
-    kubeproxy {}
-  }
-
-  authentication {
-    strategy = "x509"
-  }
-
-  authorization {
-    mode = var.kubernetes_authorization
-  }
 
   network {
     plugin = "canal"
@@ -68,6 +34,4 @@ resource "rke_cluster" "cluster" {
   ingress {
     provider = "none"
   }
-
-  addon_job_timeout = 10
 }
